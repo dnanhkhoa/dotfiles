fcf0baddcaf8ccc31a0f94cd78bba1aa7aa9c469
Khoa Duong <dnakhoa@icloud.com>
2025-03-04 23:07:43 +0700

    feat(install): add macos apps and tools

--- install_macos.sh
+++ install_macos.sh
@@ -15,6 +15,29 @@ brew update
 
 # Install packages
 brew bundle --no-lock --file=/dev/stdin <<EOF
+brew "stow"
+
+cask "cleanshot"
+cask "font-jetbrains-mono"
+cask "free-download-manager"
+cask "google-chrome"
+cask "logi-options+"
+cask "obsidian"
+cask "orbstack"
+cask "proxyman"
+cask "raycast"
+cask "screen-studio"
+cask "ssh-config-editor"
+cask "tableplus"
+cask "visual-studio-code"
+cask "vlc"
+cask "wezterm"
+
+mas "OneDrive", id: 823766827
+mas "Tailscale", id: 1475387142
+mas "Telegram", id: 747648890
+mas "Whisper Transcription", id: 1668083311
+mas "Yubico Authenticator", id: 1497506650
 EOF
 
 # Cleanup old packages
